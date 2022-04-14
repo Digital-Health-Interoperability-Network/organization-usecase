23de2c162eec763bfd42558dd26e204641851090
Sally-Builds <uzoagulujoshua@yahoo.com>
2022-04-14 14:14:21 +0100

    rm bcrypt and used bcryptjs

--- src/models/organization_model.js
+++ src/models/organization_model.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const crypto = require('crypto');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const randomstring = require('randomstring');
 const Identifier = require('./identifier_model');
 const Address = require('./address_model');
